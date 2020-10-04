11f1b25d3f8b291503f9eb6fc05ff3ba4f131030
Xiaoxun sun <sunxiaoxun@localhost>
2020-10-04 14:52:38 +0800

    The second Version of AddingMachine.java.

--- 360assignment02/src/cse360assignment02/AddingMachine.java
+++ 360assignment02/src/cse360assignment02/AddingMachine.java
@@ -1,28 +1,42 @@
+
 package cse360assignment02;
+import java.util.*;
 /*public class AddingMachine */
 public class AddingMachine {
-	
+	public static void main(String [] args)
+	{
+		
+	}
 	/* variable total */
   private int total;
+  StringBuffer history;
   /* construction */
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    history = new StringBuffer("0");
   }
   /* get value of total*/
   public int getTotal () {
-    return 0;
+    return total;
   }
   /* add value to total */
   public void add (int value) {
+	  total = total + value;
+	  history.append(" + " + value);
   }
 /* subtract a value from total */
   public void subtract (int value) {
+	  total = total - value;
+	  history.append(" - " + value);
   }
 /* print out the result */
   public String toString () {
-    return "";
+	  
+	  
+    return history.toString();
   }
 /* clear total */
   public void clear() {
+	  total = 0;
   }
 }
