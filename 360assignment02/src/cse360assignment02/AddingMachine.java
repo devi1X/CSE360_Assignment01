e57676ff4a6bb13c14a8588a9cabfa83bb561d25
Xiaoxun sun <sunxiaoxun@localhost>
2020-10-04 14:25:46 +0800

    AddingMacine

---
+++ 360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,32 @@
+package cse360assignment02;
+/*public class AddingMachine */
+public class AddingMachine {
+	/*main function */
+	public static void main(String[] args)
+	{
+		
+	}
+	/* variable total */
+  private int total;
+  /* construction */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  /* get value of total*/
+  public int getTotal () {
+    return 0;
+  }
+  /* add value to total */
+  public void add (int value) {
+  }
+/* subtract a value from total */
+  public void subtract (int value) {
+  }
+/* print out the result */
+  public String toString () {
+    return "";
+  }
+/* clear total */
+  public void clear() {
+  }
+}
